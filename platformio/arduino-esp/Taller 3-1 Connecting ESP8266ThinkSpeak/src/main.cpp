c9a146a2bfc54f030c394c0efa8f13040c0229ef
manzamb <manzamb@unicauca.edu.co>
2020-05-15 12:43:57 -0500

    arreglado programa principal

--- platformio/arduino-esp/Taller 3-1 Connecting ESP8266ThinkSpeak/src/main.cpp
+++ platformio/arduino-esp/Taller 3-1 Connecting ESP8266ThinkSpeak/src/main.cpp
@@ -1,167 +1,143 @@
-#include <Arduino.h>
+//Este skecht se ha desarrollado para activar el bombillo a partir del umbral
+//también el ventilador cuando la temperatura suba por encima de cierto umbral
+#include "Arduino.h"
 #include <ThingSpeak.h>
+
+//------------------------- Actiovar WIFI ESP8266 -----------------------
 #include <ESP8266WiFi.h>
-#include <math.h>
-#include "rgb_lcd.h"
 
-// Network Parameters
-//const char* ssid     = "GtiNetwork";
-//const char* password = "gi3z-mig1-tpkz";
-const char* ssid = "Angelonet";
-const char* password = "Marcus336";
+char ssid[] = "Yeye1";
+char password[] = "198219500";
+//-------------------------- Fin Configuración WIFI ESP8266 --------------
 
-// ThingSpeak information
-char thingSpeakAddress[] = "api.thingspeak.com";
-unsigned long channelID = 800119;
-char* readAPIKey = (char*)"69IY9W03ZXF67X5U";
-char* writeAPIKey = (char*)"7ZXS3OPHWQFL6Q1I";
-const unsigned long postingInterval = 20L * 1000L;
-unsigned int dataFieldOne = 1;                       // Field to write estado bombillo
-unsigned int dataFieldTwo = 2;                       // Field to write temperature data
-unsigned int dataFieldThree = 3;                       // Field to write tiempo
+//const int sensorluzpin = A3;    //Fotocelda Grove
+const int bombillopin = 3;      //Simulado con un led 13 en Arduino
+const int ventiladorpin = D5;    //Relay del ventilador
+const int temperaturapin = A0;  //Temperatura Grove 
 
-//RGB Global variable
-const int colorR = 0;
-const int colorG = 0;
-const int colorB = 255;
-rgb_lcd lcd;
+//Variables Globales
+int umbralLuz = 500;            //Es el umbral en el cual se enciende el bombillo
+int umbralTemperatura = 31;     //Es el umbral en el cual se enciende el ventilador
+float luminosidad;              //Toma el valor en voltaje
+float temperatura;              //Toma el valor en grados
+boolean estadoventilador=false; //false = apagado
+boolean estadobombillo = false; //false = apagado
 
-// Global variables
-// These constants are device specific.  You will need to get them from the manufacturer or determine them yourself.
-//Parametros de los pines
-const int temperaturapin = A0;
-const int ledpin = D5;
-const int umbraltemperatura = 28;
+//Métodos para encapsular las funcionalidades
+void LeerSensores(void)
+{
+   //leer el sensor de luz
+   //luminosidad = analogRead(sensorluzpin); 
 
-unsigned long lastConnectionTime = 0;
-long lastUpdateTime = 0; 
-WiFiClient client;                                  
-
-void connectWiFi(void){
-  delay(10);
-  // We start by connecting to a WiFi network  
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  //RGB comand
-  lcd.setCursor(0, 1);
-  lcd.print("Conectando a ");
-  lcd.print(ssid);
-
-  WiFi.begin(ssid, password); 
-  lcd.setCursor(0, 2); 
-  while (WiFi.status() != WL_CONNECTED)  
-  {  
-   delay(500);  
-   Serial.print(".");
-   lcd.print(".");
-  }  
-  Serial.println("");  
-  Serial.println("WiFi connected");  
-  // Print the IP address  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  lcd.print("");  
-  lcd.println("WiFi conectada");  
-  // Print the IP address  
-  lcd.print("IP address: ");
-  lcd.print( WiFi.localIP());
-
-  ThingSpeak.begin( client );
+    //recibe la temperatura para el sensor LM35
+   //temperatura = analogRead(temperaturapin);   
+   //temperatura = (5.0 * temperatura * 100.0)/1024.0; 
+ 
+   //Lee estado de sensor de Temperatura para GROVE temp
+   int B=3975; //Valor del termistor
+   temperatura = analogRead(temperaturapin); //Obtencion del valor leido
+   float resistance=(float)(1023-temperatura)*10000/temperatura; //Obtencion del valor de la resistencia
+   temperatura=1/(log(resistance/10000)/B+1/298.15)-273.15; //Calculo de la temperatura
 }
 
-
-// Use this function if you want to write a single field
-int writeTSData( long TSChannel, unsigned int TSField, float data ){
-  int  writeSuccess = ThingSpeak.writeField( TSChannel, TSField, data, writeAPIKey ); // Write the data to the channel
-  if ( writeSuccess ){
-    lcd.setCursor(0, 1);
-    lcd.print("Send ThinkSpeak");
-    Serial.println( String(data) + " written to Thingspeak." );
-    }
-    
-    return writeSuccess;
-}
-
-//use this function if you want multiple fields simultaneously
-int write2TSData( long TSChannel, unsigned int TSField1, 
-                  float field1Data,unsigned int TSField2, long field2Data,
-                  unsigned int TSField3, long field3Data ){
-
-  ThingSpeak.setField( TSField1, field1Data );
-  ThingSpeak.setField( TSField2, field2Data );
-  ThingSpeak.setField( TSField3, field3Data );
-
-  int printSuccess = ThingSpeak.writeFields( TSChannel, writeAPIKey );
-  return printSuccess;
-}
-
-
-void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  lcd.setRGB(colorR, colorG, colorB);
-  lcd.print("Iniciando ..!");
-
-  Serial.begin(9600);
-  delay(1500);
-  Serial.println("Start");
-  // prepare GPIO2
-  pinMode(ledpin, OUTPUT);
-  digitalWrite(ledpin, LOW);
-  //Conectar a la red
-  connectWiFi();
-}
-
-void loop() {
-    // Only update if posting time is exceeded
-    if (millis() - lastUpdateTime >=  postingInterval) {
-           
-        lastUpdateTime = millis();
-        int estaBombillo;
-
-        float readValue = analogRead(temperaturapin);
-        //En caso de Ser un TMP36
-        //float voltage = (readValue / 1024.0) * 3.3; 
-        //float temperatura = (voltage - .5) * 100;
-
-        //En caso de conectar una Grove
-        const int R0 = 100000;            // R0 = 100k
-        const int B = 4275;               // B value of the thermistor
-        float voltage = 1023.0/readValue-1.0;
-        voltage = R0*voltage;
-        float temperatura = 1.0/(log(voltage/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
-
-        Serial.println("ADC =  " + String( readValue )+ " Temp = "+String( temperatura ));
-        lcd.setCursor(0, 2);
-        String medida = "Temp = "+ String( temperatura );
-        lcd.print(medida);
-
-        //Cambiar estado del bombillo
-        if(temperatura > umbraltemperatura){
-            estaBombillo = 1;
-            digitalWrite(ledpin, HIGH);
-        }
-        else
-        {
-          estaBombillo = 0;
-          digitalWrite(ledpin, LOW);
-        }
-        write2TSData( channelID , dataFieldOne , temperatura , 
-                      dataFieldTwo , estaBombillo,
-                      dataFieldThree, millis());      // Write the temperature.
-    }
-}
-
-
-
-  float readTSData( long TSChannel,unsigned int TSField )
-  {
-    
-  float data =  ThingSpeak.readFloatField( TSChannel, TSField, readAPIKey );
-  Serial.println( " Data read from ThingSpeak: " + String( data, 9 ) );
-  return data;
+void ImprimirValoresSensores(void)
+{
+ //Imprimir los valores sensados
+  Serial.println("========================================");
   
+ //Temeratura
+ Serial.print("Temperatura: ");
+ Serial.print(temperatura);
+ Serial.print(" ");
+ Serial.println(" ℃ ");
+
+ //Luminosidad
+ Serial.print("Luminosidad: ");
+ Serial.print(luminosidad);
+ Serial.print(" ");
+ Serial.println(" V. ");
+
+ //Estado del Bombillo
+ if (estadobombillo == false)
+    Serial.println("Bombillo Apagado");
+ else
+    Serial.println("Bombillo Encendido");
+
+ //Estado del Ventilador
+ if (estadoventilador == false)
+    Serial.println("Ventilador Apagado");
+ else
+    Serial.println("Ventilador Encendido");
+}
+
+boolean UmbraldeTemperatura(float umbral)
+{
+  if(temperatura > umbral){
+    digitalWrite(ventiladorpin, HIGH); 
+    delay(1000);
+    return true;
+  }  
+  else{
+    digitalWrite(ventiladorpin, LOW);  
+    delay(10); 
+    return false;
+  } 
+}
+
+boolean UmbraldeLuz(float umbral)
+{
+  //Envia una señal que activa o desactiva el relay
+  if(luminosidad < umbral){
+    digitalWrite(bombillopin, HIGH);
+    delay(1000);
+    return true;
+  }   
+  else{
+    digitalWrite(bombillopin, LOW);
+    delay(10);
+    return false;  
   }
+}
+
+//metodo cliente para controlar los eventos R1 y R2
+void setup()
+{
+  //Abrir el puerto de lectura en el PC para mensajes
+  Serial.begin(115200);
+
+  //----------- Comando para Conectarse a la WIFI el ESP8266 ---------
+  Serial.println("Conectandose a la WIFI!");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectada");
+  Serial.println(WiFi.localIP());
+  //----------- Fin de conección ESP8266 -----------------------------
+
+  //Establecer los modos de los puertos
+  //pinMode(sensorluzpin, INPUT);
+  pinMode(bombillopin, OUTPUT);
+  pinMode(ventiladorpin, OUTPUT);
+  pinMode(temperaturapin, INPUT);
+  
+}
+
+//metodo repetitivo
+void loop()                    
+{
+  LeerSensores();
+  ImprimirValoresSensores();
+
+  //Verificar los umbrales
+  estadoventilador = UmbraldeTemperatura(umbralTemperatura);
+  estadobombillo = UmbraldeLuz(umbralLuz);
+
+  
+//espera en milisegundos para volver a tomar la temperatura 
+  delay(2000);
+}
