5585eabe94cabfdf0df302e69e76edc1bd99963e
manzamb <manzamb@unicauca.edu.co>
2025-03-28 06:42:35 -0500

    Actualizando Taller 3

---
+++ platformio/Talleres ESP 32/Taller 4 Creando Librerias/src/main.cpp
@@ -0,0 +1,101 @@
+//Este skecht se ha desarrollado para activar el bombillo a partir del umbral
+//también el ventilador cuando la temperatura suba por encima de cierto umbral
+//Comunicaciones: Utiliza ThingSpeak para las telecomunicaciones con ESP8266 - Entorno Arduino
+//Tambien se ha modificadopar encapsular las funcionalidades añadidas en cada nuevo taller
+#include "Arduino.h"
+#include <IoTdeviceLib.h>       //Librería con funciones de sensor - actuador
+#include <IoTcomLib.h>          //Librería con funciones de comunicación del dispositivo
+#include <DNSServer.h>          //Es necesario instalar la librería EspSoftwareSerial y Wifimanager 
+//#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+//-------------------------- Fin Configuración WIFI ESP32 --------------
+
+//Entradas digitales del ESP 32
+const int bombillopin = 18;     //Simulado con un led rojo
+const int ventiladorpin =19;   //Simulado con un led azul
+
+//Entradas Analogas del ESP 32
+const int temperaturapin = 36;  //Temperatura TMP36 
+const int potenciometro = 39;   //Poteciometro para ejemplo PWM
+const int sensorluzpin = 35;    //Fotocelda que 
+
+//Variables Globales
+int umbralLuz = 500;                                //Es el umbral en el cual se enciende el bombillo
+int umbralTemperatura = 28;                         //Es el umbral en el cual se enciende el ventilador
+float luminosidad;                                  //Toma el valor en voltaje
+float temperatura;                                  //Toma el valor en grados
+boolean estadoventilador =false;                    //false = apagado
+boolean estadobombillo = false;                     //false = apagado
+int nummedicion = 0;                                //Establece el número consecutivo de observacion hecha
+const unsigned long postingInterval = 20L * 1000L;  //Establece cada cuanto se envia a ThingSpeak
+unsigned long lastConnectionTime = 0;               //Para controlar el tiempo de generar nueva medición
+long lastUpdateTime = 0;                            //Momento de la última actualización
+
+//metodo cliente para controlar los eventos R1 y R2
+void setup()
+{
+  //Abrir el puerto de lectura en el PC para mensajes
+  Serial.begin(115200);
+
+  // Resolución Sensores ADC
+  //Resolucion de los puesrtos ADC
+  analogReadResolution(10);
+  
+  //ConectarRed("Redmi","Marcus336");  //Conectar con datos desde el programa
+  //-----Comando para Conectarse y configurar desde el Celular--------
+  // Creamos una instancia de la clase WiFiManager
+  WiFiManager wifiManager;
+
+  // Descomentar para resetear configuración - Hacer el ejercicio con el celular
+  // todas las veces.
+  //wifiManager.resetSettings();
+
+  // Creamos AP y portal para configurar desde el Celular
+  wifiManager.autoConnect("ESP8266Temp");
+ 
+  Serial.println("!Ya estás conectado¡");
+  //----------- Fin de conección ESP8266 -----------------------------
+
+  //Establecer los modos de los puertos
+  pinMode(sensorluzpin, INPUT);
+  pinMode(bombillopin, OUTPUT);
+  pinMode(ventiladorpin, OUTPUT);
+  pinMode(temperaturapin, INPUT);
+
+  //inicializar aqui thingspeak
+  InicializarThingSpeak();
+}
+
+void loop()                    
+{
+  // Solamente actualiza si el tiempo de publicación es excedido
+  if (millis() - lastUpdateTime >=  postingInterval) {
+      lastUpdateTime = millis();
+
+      //LeerSensores
+      temperatura = LeerTemperatura(temperaturapin,Tmp36,3.3);
+      luminosidad = LeerLuminosidad(sensorluzpin);
+
+      //Imprimir Valores Sensores y Actuadores 
+      Serial.print("=========== Medición No.: ");
+      Serial.print(nummedicion++);
+      Serial.println(" ============");
+      ImprimirValorSensor(temperatura,"Temperatura Sala"," ℃ ");
+      //Se verifica umbral antes de imprimier el estado del actuador
+      estadoventilador = UmbralMayorDeSensorActuador(temperatura,umbralTemperatura,ventiladorpin);
+      ImprimirEstadoActuador(ventiladorpin,"Ventilador Sala");
+      ImprimirValorSensor(luminosidad,"Luminosidad Sala"," V. ");
+      //Se verifica umbral antes de imprimier el estado del actuador
+      estadobombillo = UmbralMenorDeSensorActuador(luminosidad,umbralLuz,bombillopin);
+      ImprimirEstadoActuador(bombillopin,"Bobillo Sala");
+      Serial.println("========================================");
+
+      //Enviar los Datos a ThinkSpeak
+      
+      EnviarThingSpeakVariosDatos(1 , temperatura , 
+                                  2 , estadobombillo,
+                                  3 , estadoventilador,
+                                  4, millis());     
+    }
+}
