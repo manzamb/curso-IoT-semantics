09bec66773ee7c3ceaa204295bdbea6da602adc7
manzamb <manzamb@unicauca.edu.co>
2025-10-17 15:31:45 -0500

    cambios menores

--- platformio/Talleres ESP 32/Taller 2 Pulse With Modulation/src/main.cpp
+++ platformio/Talleres ESP 32/Taller 2 Pulse With Modulation/src/main.cpp
@@ -4,6 +4,7 @@
 #define LEDC_TIMER_13_BIT 13
 #define LEDC_BASE_FREQ 5000
 #define LED_PIN 18
+
 int brightness = 0;
 int fadeAmount = 5;
 void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255)
@@ -11,6 +12,8 @@ void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255)
  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
  ledcWrite(channel, duty);
 }
+
+
 void setup() {
  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
  ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
